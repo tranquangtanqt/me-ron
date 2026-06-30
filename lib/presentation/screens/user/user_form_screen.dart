1b2d02043dfdeab9ec5f58caa80e1ca465168942
tranquangtanqt <tranquangtanqt1990@gmail.com>
2026-07-01 01:46:21 +0700

    Kiểm tra trùng tên khách hàng

--- lib/presentation/screens/user/user_form_screen.dart
+++ lib/presentation/screens/user/user_form_screen.dart
@@ -5,7 +5,6 @@ import 'package:go_router/go_router.dart';
 
 import '../../../core/themes/app_sizes.dart';
 import '../../providers/address/address_notifier.dart';
-import '../../providers/category/category_notifier.dart';
 import '../../providers/user/user_form_notifier.dart';
 import '../../widgets/app_button.dart';
 import '../../widgets/app_dialog.dart';
@@ -55,45 +54,59 @@ class _UserFormScreenState extends ConsumerState<UserFormScreen> {
   }
 
   void createUser() async {
-    var res = await AppDialog.showProgress(() {
-      return ref.read(userFormNotifierProvider.notifier).createUser();
-    });
+    try {
+      var res = await AppDialog.showProgress(() {
+        return ref.read(userFormNotifierProvider.notifier).createUser();
+      });
 
-    if (res.isSuccess) {
+      if (res.isSuccess) {
+        if (!mounted) return;
+        context.pop();
+        AppSnackBar.show('Thêm mới dữ liệu thành công');
+      } else {
+        AppDialog.showError(error: res.error?.toString());
+      }
+    } catch (e) {
       if (!mounted) return;
-      // context.go('/user');
-      context.pop();
-      AppSnackBar.show('Thêm mới dữ liệu thành công');
-    } else {
-      AppDialog.showError(error: res.error?.toString());
+      AppDialog.showError(error: e.toString());
     }
   }
 
   void updateUser() async {
-    var res = await AppDialog.showProgress(() {
-      return ref.read(userFormNotifierProvider.notifier).updatedUser(widget.id!);
-    });
+    try {
+      var res = await AppDialog.showProgress(() {
+        return ref.read(userFormNotifierProvider.notifier).updatedUser(widget.id!);
+      });
 
-    if (res.isSuccess) {
+      if (res.isSuccess) {
+        if (!mounted) return;
+        context.pop();
+        AppSnackBar.show('Cập nhật dữ liệu thành công!');
+      } else {
+        AppDialog.showError(error: res.error?.toString());
+      }
+    } catch (e) {
       if (!mounted) return;
-      context.pop();
-      AppSnackBar.show('Cập nhật dữ liệu thành công!');
-    } else {
-      AppDialog.showError(error: res.error?.toString());
+      AppDialog.showError(error: e.toString());
     }
   }
 
   void deleteUser() async {
-    var res = await AppDialog.showProgress(() {
-      return ref.read(userFormNotifierProvider.notifier).deleteUser(widget.id!);
-    });
+    try {
+      var res = await AppDialog.showProgress(() {
+        return ref.read(userFormNotifierProvider.notifier).deleteUser(widget.id!);
+      });
 
-    if (res.isSuccess) {
+      if (res.isSuccess) {
+        if (!mounted) return;
+        context.pop();
+        AppSnackBar.show('Xóa dữ liệu thành công!');
+      } else {
+        AppDialog.showError(error: res.error?.toString());
+      }
+    } catch (e) {
       if (!mounted) return;
-      context.pop();
-      AppSnackBar.show('Xóa dữ liệu thành công!');
-    } else {
-      AppDialog.showError(error: res.error?.toString());
+      AppDialog.showError(error: e.toString());
     }
   }
 
@@ -111,6 +124,12 @@ class _UserFormScreenState extends ConsumerState<UserFormScreen> {
       appBar: AppBar(
         title: Text(widget.id == null ? 'Thêm khách hàng' : 'Chỉnh sửa khách hàng'),
         titleSpacing: 0,
+        shadowColor: Colors.transparent,
+        actions: [_CreateOrUpdateButton(
+          id: widget.id,
+          onCreate: createUser,
+          onUpdated: updateUser,
+        ),],
       ),
       body: !isLoaded
           ? const AppProgressIndicator()
@@ -134,11 +153,6 @@ class _UserFormScreenState extends ConsumerState<UserFormScreen> {
                   _NoteField(
                     controller: noteController,
                     onChanged: notifier.onChangedNote,
-                  ),
-                  _CreateOrUpdateButton(
-                    id: widget.id,
-                    onCreateUser: createUser,
-                    onUpdateUser: updateUser,
                   ),
                   _DeleteButton(
                     id: widget.id,
@@ -247,13 +261,13 @@ class _NoteField extends StatelessWidget {
 
 class _CreateOrUpdateButton extends ConsumerWidget {
   final int? id;
-  final VoidCallback onCreateUser;
-  final VoidCallback onUpdateUser;
+  final VoidCallback onCreate;
+  final VoidCallback onUpdated;
 
   const _CreateOrUpdateButton({
     required this.id,
-    required this.onCreateUser,
-    required this.onUpdateUser,
+    required this.onCreate,
+    required this.onUpdated,
   });
 
   @override
@@ -265,17 +279,46 @@ class _CreateOrUpdateButton extends ConsumerWidget {
     );
 
     return Padding(
-      padding: const EdgeInsets.only(top: AppSizes.padding * 1.5),
-      child: AppButton(
-        text: 'Lưu',
-        enabled: isFormValid,
-        onTap: () {
-          if (id != null) {
-            onUpdateUser();
-          } else {
-            onCreateUser();
-          }
-        },
+      padding: const EdgeInsets.only(right: AppSizes.padding),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // ===== SAVE BUTTON =====
+          AppButton(
+            height: 26,
+            borderRadius: BorderRadius.circular(4),
+            padding: const EdgeInsets.symmetric(
+              horizontal: AppSizes.padding / 2,
+            ),
+            buttonColor: Theme.of(context).colorScheme.surfaceContainer,
+            onTap: () {
+              if (id != null) {
+                onUpdated();
+              } else {
+                onCreate();
+              }
+            },
+            enabled: isFormValid,
+            child: Row(
+              children: [
+                Icon(
+                  Icons.save,
+                  size: 12,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                const SizedBox(width: AppSizes.padding / 4),
+                Text(
+                  'Lưu',
+                  style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
